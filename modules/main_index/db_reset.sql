1dff89676c3197c8ade86de2fc7f1c1388057ece
samedcildir <samedcildir@gmail.com>
2025-06-13 17:50:33 +0300

    removed old brc20 indexer and main indexer

--- modules/main_index/db_reset.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists ord_transfers;
-drop table if exists ord_number_to_id;
-drop table if exists ord_content;
-drop table if exists block_hashes;
-drop table if exists ord_indexer_reorg_stats;
-drop table if exists ord_indexer_work_stats;
-drop table if exists ord_indexer_version;
-drop table if exists ord_network_type;
-drop table if exists ord_transfer_counts;
